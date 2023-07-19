0bed94f1f8936ccbe06d0fe20ed099566eb581c7
eugene <eugenemuasya395@gmail.com>
2023-07-19 16:22:44 +0300

    login-sign up  files

---  login page/scripts/login.js
+++
@@ -1,9 +0,0 @@
-function signup() {
-  document.getElementById('loginform').style.display = 'none';
-  document.getElementById('signform').style.display = 'block';
-}
-
-function login() {
-  document.getElementById('loginform').style.display = 'block';
-  document.getElementById('signform').style.display = 'none';
-}
